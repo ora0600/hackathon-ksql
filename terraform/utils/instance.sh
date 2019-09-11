c3c96282313dbf13380b295e69b3c597564da111
CM <38393728+ora0600@users.noreply.github.com>
2019-09-11 16:34:02 +0200

    Update instance.sh

--- terraform/utils/instance.sh
+++ terraform/utils/instance.sh
@@ -6,6 +6,13 @@ yum install java-1.8.0-openjdk-devel.x86_64 -y
 # install docker
 yum install -y docker
 usermod -a -G docker ec2-user
+# set environment
+echo vm.max_map_count=262144 >> /etc/sysctl.conf
+sysctl -w vm.max_map_count=262144
+echo "    *       soft  nofile  65535
+    *       hard  nofile  65535" >> /etc/security/limits.conf
+sed -i -e 's/1024:4096/65536:65536/g' /etc/sysconfig/docker
+# enable docker    
 service docker start
 chkconfig docker on
 curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
