bfb3bf80e4c88846bbcf0e2017886371c36b9d48
CM <38393728+ora0600@users.noreply.github.com>
2019-09-16 12:09:17 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,38 @@
+resource "aws_security_group" "atmfraud" {
+  name        = "atmfraudSecGroup"
+  description = "ATM Fraud Det. Sec. Group"
+
+  # SSH access from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTP access from the VPC
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "ATMFraudDetection" {
   ami           = "${data.aws_ami.ami.id}"
   count         = var.instance_count
   instance_type = var.instance_type_resource
   key_name      = var.ssh_key_name
+  vpc_security_group_ids = ["${aws_security_group.atmfraud.id}"]
   user_data = data.template_file.ATMFraudDetection_instance.rendered
 
   root_block_device {
