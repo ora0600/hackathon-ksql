6843af0e6f4ab198a799ed46d8fd78fc9d2be2c4
CM <38393728+ora0600@users.noreply.github.com>
2019-11-07 13:43:01 +0100

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,3 +8,7 @@ output "SSH" {
 output "SSH_Tunnel" {
   value = tonumber(var.instance_count) >= 1 ? "With Tunnel: ssh -i hackathon-temp-key.pem -N -L 9022:${join(",", formatlist("%s", aws_instance.ATMFraudDetection.*.private_dns),)}:9021 ec2-user@${join(",", formatlist("%s", aws_instance.ATMFraudDetection.*.public_ip),)}" : "Confluent Cloud Platform on AWS is disabled"
 }  
+
+output "C3" {
+  value = tonumber(var.instance_count) >= 1 ? "Control Center  Access: http://{join(",",formatlist("%s", aws_instance.ATMFraudDetection.*.public_ip),)} " : "Confluent Cloud Platform on AWS is disabled" 
+}
